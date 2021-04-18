9578ea6652451a154dd96ec261e2d50bd86950c1
Braedon <Braedonww@gmail.com>
2021-04-18 18:14:44 +1000

    Update send-pingback.ts

--- packages/analytics/src/send-pingback.ts
+++ packages/analytics/src/send-pingback.ts
@@ -7,13 +7,4 @@ const environment = gl?.GIPHY_PINGBACK_URL || 'https://pingback.giphy.com'
 const pingBackUrl = `${environment}/v2/pingback?apikey=l0HlIwPWyBBUDAUgM`
 
 export const sendPingback = (events: PingbackEvent[]) => {
-    const headers = getGiphySDKRequestHeaders()
-    /* istanbul ignore next */
-    headers?.set('Content-Type', 'application/json')
-    Logger.debug(`Pingback session`, events)
-    return fetch(pingBackUrl, {
-        method: 'POST',
-        body: JSON.stringify({ events }),
-        headers,
-    })
 }
